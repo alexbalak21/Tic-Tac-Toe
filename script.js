3200adaa847970f67bfa7cd6e705ee6d16b121e6
alexblack24 <alex.black@mail.com>
2024-05-04 17:31:36 +0200

    winining conditions implemented, computer play randomply

--- script.js
+++ script.js
@@ -2,6 +2,10 @@ const gridTable = document.getElementById("grid")
 const playButton = document.getElementById("play")
 playButton.addEventListener("click", play)
 
+let first = true
+let symbol = Math.random() < 0.5 ? "X" : "O"
+let turn = 0
+
 const grid = new Array(3)
 for (let i = 0; i < 3; i++) grid[i] = new Array(3)
 const avalableCells = []
@@ -12,121 +16,68 @@ for (let y = 0; y < 3; y++) {
         const td = document.createElement("td")
         tr.appendChild(td)
         grid[y][x] = td
+        td.addEventListener("click", takeCell)
         avalableCells.push(td)
     }
     gridTable.appendChild(tr)
 }
 
-function play() {
-    for (let y = 0; y < 3; y++) for (let x = 0; x < 3; x++) grid[y][x].addEventListener("click", takeCell)
-    console.log("PLAYING")
-}
+if (symbol === "O") computerPlay()
 
 function takeCell() {
-    if (this.innerText === "") {
-        const cellIndex = avalableCells.indexOf(this)
-        avalableCells.splice(cellIndex, 1)
-        console.log(avalableCells)
-        this.innerText = "X"
-    } else console.log("NOT ALLOWED")
-    let win = checkForWin()
-    if (win !== "") return console.log(win)
-    else copmputerPlay()
+    let win = false
+    if (this.innerText !== "") return
+    this.innerText = symbol
+    const cellIndex = avalableCells.indexOf(this)
+    this.removeEventListener("click", takeCell)
+    avalableCells.splice(cellIndex, 1)
+    turn++
+    if (turn >= 5) win = checkForWin()
+    if (win) {
+        for (let cell of avalableCells) cell.removeEventListener("click", takeCell)
+        return console.log("WINNER ", symbol)
+    }
+    if (symbol === "X") {
+        symbol = "O"
+        computerPlay()
+    } else symbol = "X"
 }
 
-function rndInteger(min, max) {
+function randInteger(min = 0, max = 10) {
     return Math.floor(Math.random() * (max - min)) + min
 }
 
-function copmputerPlay() {
-    const cellIndex = rndInteger(0, avalableCells.length - 1)
-    avalableCells[cellIndex].innerText = "O"
-    avalableCells.splice(cellIndex, 1)
-    let win = checkForWin()
-    if (win !== "") return console.log(win)
-    else play()
+function computerPlay() {
+    const index = randInteger(0, avalableCells.length - 1)
+    avalableCells[index].click()
 }
 
 function checkForWin() {
-    let win = 0
-    for (let y = 0; y < 3; y++) {
-        win = checkLine(y)
-        if (win === 1) return "PLAYER X WINS"
-        if (win === 2) return "PLAYER O WINS"
-    }
-
-    for (let x = 0; x < 3; x++) {
-        win = checkColumn(x)
-        if (win === 1) return "PLAYER X WINS"
-        if (win === 2) return "PLAYER O WINS"
-    }
-    win = checkDiag()
-    if (win === 1) return "PLAYER X WINS"
-    if (win === 2) return "PLAYER O WINS"
-    return ""
+    let win = false
+    for (let line = 0; line < 3; line++) if (checkLine(line)) return true
+    for (let column = 0; column < 3; column++) if (checkColumn(column)) return true
+    if (checkDiags()) return true
+    return win
 }
 
 function checkLine(y) {
-    let takenCellsX = 0
-    let takenCellsO = 0
-    for (let x = 0; x < 3; x++) {
-        switch (grid[y][x].innerText) {
-            case "X":
-                takenCellsX++
-                break
-            case "X":
-                takenCellsO++
-                break
-        }
-    }
-    if (takenCellsX === 3) return 1
-    else if (takenCellsO === 3) return 2
-    else return 0
+    let points = 0
+    for (let x = 0; x < 3; x++) points += grid[y][x].innerText === symbol ? 1 : 0
+    return points === 3
 }
+
 function checkColumn(x) {
-    let takenCellsX = 0
-    let takenCellsO = 0
-    for (let y = 0; y < 3; y++) {
-        switch (grid[y][x].innerText) {
-            case "X":
-                takenCellsX++
-                break
-            case "X":
-                takenCellsO++
-                break
-        }
-    }
-    if (takenCellsX === 3) return 1
-    else if (takenCellsO === 3) return 2
-    else return 0
+    let points = 0
+    for (let y = 0; y < 3; y++) points += grid[y][x].innerText === symbol ? 1 : 0
+    return points === 3
 }
-function checkDiag() {
-    let takenCellsX = 0
-    let takenCellsO = 0
-    for (let i = 0; i < 3; i++) {
-        switch (grid[i][i].innerText) {
-            case "X":
-                takenCellsX++
-                break
-            case "X":
-                takenCellsO++
-                break
-        }
-    }
-    if (takenCellsX === 3) return 1
-    else if (takenCellsO === 3) return 2
-    takenCellsX = 0
-    takenCellsO = 0
-    for (let j = 2; j >= 0; j--)
-        switch (grid[j][j].innerText) {
-            case "X":
-                takenCellsX++
-                break
-            case "X":
-                takenCellsO++
-                break
-        }
-    if (takenCellsX === 3) return 1
-    else if (takenCellsO === 3) return 2
-    return 0
+
+function checkDiags() {
+    let points = 0
+    for (let i = 0; i < 3; i++) points += grid[i][i].innerText === symbol ? 1 : 0
+    if (points === 3) return true
+
+    points = 0
+    for (let j = 0; j >= 0; j--) points += grid[j][j].innerText === symbol ? 1 : 0
+    return points === 3
 }
