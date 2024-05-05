fc19f5faaf5f4eb3f0959ef3a2d4650d8b9bcf54
alexblack24 <alex.black@mail.com>
2024-05-06 00:43:48 +0200

    logic second step of the logic almost done.

--- script.js
+++ script.js
@@ -49,16 +49,64 @@ function randInteger(min = 0, max = 10) {
 
 function computerPlay() {
     const index = randInteger(0, avalableCells.length - 1)
-    avalableCells[index].click()
+    if (turn === 0) grid[1][1].click()
+    if (turn === 1) {
+        if (grid[1][1].innerText === "X") playCorner()
+        else grid[1][1].click()
+    } else {
+        prioritise2Points()
+        // avalableCells[index].click()
+    }
 }
 
 function prioritise2Points() {
+    console.log("----------------------prioritise2Points-------------")
+    if (turn < 3) return
+    console.log("TURN 3 OR MORE")
+    let points = 0
     for (let y = 0; y < 3; y++) {
-        let points = 0
+        points = checkLine(y)
+        if (points === 2) {
+            fillTheRemainingCell(y, 0)
+            return
+        }
+        points = 0
         for (let x = 0; x < 3; x++) {
-            points += grid[y][x].innerText === symbol ? 1 : 0
+            points = checkColumn(x)
+            if (points === 2) {
+                fillTheRemainingCell(0, x)
+                return
+            }
         }
     }
+}
+
+function fillTheRemainingCell(y = 0, x = 0) {
+    if (x === 0)
+        for (x; x < 3; x++)
+            if (grid[y][x].innerText === "") {
+                console.log(grid[y][x])
+                grid[y][x].click()
+                break
+            }
+    if (y === 0)
+        for (y; y < 3; y++)
+            if (grid[y][x].innerText === "") {
+                console.log(grid[y][x])
+                grid[y][x].click()
+                break
+            }
+}
+
+function playCorner() {
+    const corners = [
+        [0, 0],
+        [0, 2],
+        [2, 2],
+        [2, 0],
+    ]
+    const corner = corners[randInteger(0, 3)]
+    grid[corner[0]][corner[1]].click()
 }
 
 function checkForWin() {
@@ -69,15 +117,18 @@ function checkForWin() {
     return win
 }
 
+//NEED TO ADD SYMBOL TO THE function.
 function checkLine(y) {
     let points = 0
     for (let x = 0; x < 3; x++) points += grid[y][x].innerText === symbol ? 1 : 0
+    console.log("SYMBOL ", symbol, " LINE ", y, " POINTS ", points)
     return points
 }
 
 function checkColumn(x) {
     let points = 0
     for (let y = 0; y < 3; y++) points += grid[y][x].innerText === symbol ? 1 : 0
+    console.log("SYMBOL ", symbol, "COLUM ", x, " - POINTS ", points)
     return points
 }
 
