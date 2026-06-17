45476d768f0bdf11a3ef361cef62ad5c6b390a7b
alexbalak21 <alex.balak@outlook.com>
2026-06-17 21:52:31 +0200

    changed the name and included a screenshot

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ export default function App() {
     <div className="min-h-screen flex items-center justify-center bg-gray-900 text-white">
       <div className="text-center space-y-6">
         <h1 className="text-4xl font-bold">
-          Tic Tac Toe Game <span className="text-green-400">In React</span>
+          Tic Tac Toe Game
         </h1>
 
         <Board />
